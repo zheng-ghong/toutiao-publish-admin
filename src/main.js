3e5d1730970a6212d89d78893f183ff54e0d002d
LittleLion <8338992+lionczx@user.noreply.gitee.com>
2021-06-18 20:41:51 +0800

    18.03.01

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
+// 项目启动入口
 import Vue from 'vue'
 import App from './App.vue'
+// 加载Element组件库及其样式
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 加载全局样式
+import './style/index.less'
+import router from './router'
 
+// 全局注册Element组件库
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
